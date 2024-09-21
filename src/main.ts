8cf2c770256d2b4ee2091e3069ba30bcaca20155
Mardix <mcx2082@gmail.com>
2024-09-21 03:04:52 -0400

    fix setup layouts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import App from '@/App.vue';
 import { ViteSSG } from 'vite-ssg';
-
 import '@/styles/index.css';
 import { ViteSetupModule } from './types/ViteSetupModule';
 import { extendedRoutes } from '@/router';
+import { setupLayouts } from 'virtual:generated-layouts'
 
 export const createApp = ViteSSG(
 	App,
-	{ routes: extendedRoutes },
+	{ routes: setupLayouts(extendedRoutes) },
 	async ctx => {
 		Object.values(
 			import.meta.glob<{ install: ViteSetupModule }>('./modules/*.ts', {
